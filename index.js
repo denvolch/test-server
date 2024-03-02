0a02cbf17c250346aa145895bacc40cc268de154
denvolch <wolkov.web@yandex.ru>
2024-03-02 18:17:19 +0300

    add handlers for unknown endpoint and invalid database queries

--- index.js
+++ index.js
@@ -3,18 +3,25 @@ const express = require('express')
 const cors = require('cors')
 const Note = require('./models/note')
 
-// if (process.argv.length < 3) {
-//     console.log('give password as argument')
-//     process.exit(1)
-// }
-
-const password = process.argv[2]
-
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
+
+const unknownEndpoint = (req, res) => res.status(404).send({ error: 'unknown endpoint' })
+app.use(unknownEndpoint)
+
+const errorHandler = (err, req, res, next) => {
+  console.log(err.message)
+
+  if (err.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(err)
+}
+app.use(errorHandler)
 
 app.get('/', (req, res) => {
   res.send('<h1>Loading...</h1>')
@@ -28,29 +35,19 @@ app.get('/api/notes', (req, res) => {
     })
 })
 
-app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find(note => {
-    console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-    return note.id === id
-  })
-  if (note) {
-    console.log(note)
-    res.json(note)
-  } else {
-    res.statusMessage = "This Note is not created on server";
-    res.status(404).end()
-  }
+app.get('/api/notes/:id', (req, res, next) => {
+  Note
+    .findById(req.params.id)
+    .then(note => {
+      if (note) {
+        console.log(note)
+        res.json(note)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(err => next(err))
 })
-
-const generatedId = (arr) => {
-  if (arr) {
-    const maxId = arr.length > 0
-      ? Math.max(...arr.map(n => Number(n.id)))
-      : 0
-    return maxId + 1
-  }
-}
 
 app.post('/api/notes/', (req, res) => {
   const body = req.body
@@ -61,15 +58,16 @@ app.post('/api/notes/', (req, res) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: Boolean(body.important) || false,
-    id: generatedId(notes)
-  }
-
-  console.log(note)
-  notes = notes.concat(note)
-  res.json(note)
+    important: body.important || false
+  })
+  note
+    .save()  
+    .then(result => {
+      console.log('new note is saved')
+      res.json(result)
+    })
 })
 
 app.delete('/api/notes/:id', (req, res) => {
@@ -83,6 +81,3 @@ const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-// app.listen(PORT)
-
-// console.log(`Server running on port ${PORT}`)
--- models/note.js
+++ models/note.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
-console.log('connecting to', url)
+// console.log('connecting to', url)
 
 mongoose.set('strictQuery', false)
 
