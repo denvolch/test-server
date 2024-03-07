346c89cf0c12216d54be24037aed22de1d2afbfd
denvolch <wolkov.web@yandex.ru>
2024-03-07 09:41:14 +0300

    test: add simple tests
    prepare for testing: install cross-env libs to run
    code cross platform, and add simple functions for
    testing

---
+++ utils/for_testing.js
@@ -0,0 +1,16 @@
+const reverse = string => {
+    return string
+        .split('')
+        .reverse()
+        .join('')
+}
+
+const average = array => {
+    const reducer = (sum, item) => sum + item
+
+    return !(array.length === 0)
+        ? array.reduce(reducer, 0) / array.length
+        : 0
+}
+
+module.exports = { reverse, average, }
