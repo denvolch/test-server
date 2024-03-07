78c0af542c596a4f9cb9bcea4ceae3c9454839cc
denvolch <wolkov.web@yandex.ru>
2024-03-07 22:38:53 +0300

    refactor: api functions
    install express-async-errors and clear
    try-catch constructs in routes

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,35 +1,26 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', async (req, res, next) => {
-  try {
-    const notes = await Note.find({})
-    res.json(notes)
-  } catch(expection) {
-    next(expection)
+notesRouter.get('/', async (req, res) => {
+  const notes = await Note.find({})
+  res.json(notes)
+})
+
+notesRouter.get('/:id', async (req, res) => {
+  const note = await Note.findById(req.params.id)
+
+  if (note) {
+    res.status(200).json(note)
+  } else {
+    res.status(404).end()
   }
 })
 
-notesRouter.get('/:id', async (req, res, next) => {
-  try {
-    const note = await Note.findById(req.params.id)
-    if (note) {
-      res.status(200).json(note)
-    } else {
-      res.status(404).end()
-    }
-  } catch(expection) {
-    next(expection)
-  }
-})
-
-notesRouter.post('/', async (req, res, next) => {
+notesRouter.post('/', async (req, res) => {
   const body = req.body
 
   if (!body.content) {
-    return res.status(400).json({
-      error: 'content missing'
-    })
+    return res.status(400).json({ error: 'content missing' })
   }
 
   const note = new Note({
@@ -37,41 +28,31 @@ notesRouter.post('/', async (req, res, next) => {
     important: body.important || false
   })
 
-  try {
-    const savedNote = await note.save()
-    res.status(201).json(savedNote)
-  } catch(expection) {
-    next(expection)
-  }
+  const savedNote = await note.save()
+  res.status(201).json(savedNote)
 })
 
-notesRouter.delete('/:id', async (req, res, next) => {
-  try {
-    await Note.findByIdAndDelete(req.params.id)
-    res.status(204).end()
-  } catch(expection) {
-    next(expection)
-  }
+notesRouter.delete('/:id', async (req, res) => {
+  await Note.findByIdAndDelete(req.params.id)
+  res.status(204).end()
 })
 
-notesRouter.put('/:id', (req, res, next) => {
+notesRouter.put('/:id', async (req, res) => {
   const note = {
     content: req.body.content,
     important: req.body.important,
   }
 
-  Note
-    .findByIdAndUpdate(
-      req.params.id,
-      note,
-      {
-        new: true,
-        runValidators: true,
-        context: 'query'
-      }
-    )
-    .then(updatedNote => res.json(updatedNote))
-    .catch(err => next(err))
+  const updatedNote = await Note.findByIdAndUpdate(
+    req.params.id,
+    note,
+    {
+      new: true,
+      runValidators: true,
+      context: 'query'
+    }
+  )
+  res.json(updatedNote)
 })
 
 module.exports = notesRouter
