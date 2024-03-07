579bc096be5636a96de4683b734438958c50178a
denvolch <wolkov.web@yandex.ru>
2024-03-07 21:08:18 +0300

    refactor: tested routes to use async/await

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,30 +1,29 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (req, res, next) => {
-  Note
-    .find({})
-    .then(notes => {
-      res.json(notes)
-    })
-    .catch(err => next(err))
+notesRouter.get('/', async (req, res, next) => {
+  try {
+    const notes = await Note.find({})
+    res.json(notes)
+  } catch(expection) {
+    next(expection)
+  }
 })
 
-notesRouter.get('/:id', (req, res, next) => {
-  Note
-    .findById(req.params.id)
-    .then(note => {
-      if (note) {
-        console.log(note)
-        res.json(note)
-      } else {
-        res.status(404).end()
-      }
-    })
-    .catch(err => next(err))
+notesRouter.get('/:id', async (req, res, next) => {
+  try {
+    const note = await Note.findById(req.params.id)
+    if (note) {
+      res.status(200).json(note)
+    } else {
+      res.status(404).end()
+    }
+  } catch(expection) {
+    next(expection)
+  }
 })
 
-notesRouter.post('/', (req, res, next) => {
+notesRouter.post('/', async (req, res, next) => {
   const body = req.body
 
   if (!body.content) {
@@ -37,20 +36,22 @@ notesRouter.post('/', (req, res, next) => {
     content: body.content,
     important: body.important || false
   })
-  note
-    .save()
-    .then(result => {
-      console.log('new note is saved')
-      res.status(201).json(result)
-    })
-    .catch(err => next(err))
+
+  try {
+    const savedNote = await note.save()
+    res.status(201).json(savedNote)
+  } catch(expection) {
+    next(expection)
+  }
 })
 
-notesRouter.delete('/:id', (req, res, next) => {
-  Note
-    .findByIdAndDelete(req.params.id)
-    .then(() => res.status(204).end())
-    .catch(err => next(err))
+notesRouter.delete('/:id', async (req, res, next) => {
+  try {
+    await Note.findByIdAndDelete(req.params.id)
+    res.status(204).end()
+  } catch(expection) {
+    next(expection)
+  }
 })
 
 notesRouter.put('/:id', (req, res, next) => {
