67d48ec0ad5e471c9270618571985072cedd5712
denvolch <wolkov.web@yandex.ru>
2024-03-10 14:44:03 +0300

    test: add tests for crating note and get it by id
    implemented several tests for api funcionality
    also small refactoring of tests for better
    readabilyt of output

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -8,103 +8,105 @@ const api = supertest(app)
 
 const Note = require('../models/note')
 
-beforeEach(async () => {
-  await Note.deleteMany({})
-  console.log('cleared')
+describe('initializing and getting all notes', () => {
+  beforeEach(async () => {
+    await Note.deleteMany({})
 
-  const noteObj = helper.initialNotes.map(note => new Note(note))
-  const promiseArr = noteObj.map(note => {
-    console.log('saved')
-    return note.save()})
-  await Promise.all(promiseArr)
+    const noteObj = helper.initialNotes.map(note => new Note(note))
+    const promiseArr = noteObj.map(note => note.save())
 
-  console.log('done')
-})
+    await Promise.all(promiseArr)
+  })
 
-test('notes are returned as json', async () => {
-  console.log('entered test')
+  test('notes are returned as json', async () => {
+    await api
+      .get('/api/notes')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-  await api
-    .get('/api/notes')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+  test('there are two notes', async () => {
+    const res = await api.get('/api/notes')
 
-test('there are two notes', async () => {
-  const res = await api.get('/api/notes')
+    assert.strictEqual(res.body.length, helper.initialNotes.length)
+  })
 
-  assert.strictEqual(res.body.length, helper.initialNotes.length)
-})
+  test('the first note is about HTTP methods', async () => {
+    const res = await api.get('/api/notes')
 
-test('the first note is about HTTP methods', async () => {
-  const res = await api.get('/api/notes')
+    const contents = res.body.map(e => e.content)
+    assert.strictEqual(contents.includes('HTML is easy'), true)
+  })
 
-  const contents = res.body.map(e => e.content)
-  assert.strictEqual(contents.includes('HTML is easy'), true)
-})
+  describe('getting and view a specific note', () =>{
+    test('a specific note can be viewed', async () => {
+      const notesAtStart = await helper.noteInDb()
 
-test('a valid note can be added', async () => {
-  const newNote = {
-    content: 'async/await simplifies making async calls',
-    important: true,
-  }
+      const noteToView = notesAtStart[0]
 
-  await api
-    .post('/api/notes')
-    .send(newNote)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+      const resultNote = await api
+        .get(`/api/notes/${noteToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
 
-  const notesAtEnd = await helper.noteInDb()
-  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length + 1)
+      assert.deepStrictEqual(resultNote.body, noteToView)
+    })
+  })
 
-  const contents = await notesAtEnd.map(n => n.content)
-  assert(contents.includes('async/await simplifies making async calls'))
-})
+  describe('adding a new note', () => {
+    test('note w/o content isn\'t added', async () => {
+      const newNote = {
+        important: true,
+      }
 
-test('note w/o content isn\'t added', async () => {
-  const newNote = {
-    important: true,
-  }
+      await api
+        .post('/api/notes')
+        .send(newNote)
+        .expect(400)
 
-  await api
-    .post('/api/notes')
-    .send(newNote)
-    .expect(400)
+      const notesAtEnd = await helper.noteInDb()
+      assert.strictEqual(notesAtEnd.length, helper.initialNotes.length)
+    })
 
-  const notesAtEnd = await helper.noteInDb()
-  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length)
-})
+    test('a valid note can be added', async () => {
+      const newNote = {
+        content: 'async/await simplifies making async calls',
+        important: true,
+      }
 
-test('a specific note can be viewed', async () => {
-  const notesAtStart = await helper.noteInDb()
+      await api
+        .post('/api/notes')
+        .send(newNote)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  const noteToView = notesAtStart[0]
+      const notesAtEnd = await helper.noteInDb()
+      assert.strictEqual(notesAtEnd.length, helper.initialNotes.length + 1)
 
-  const resultNote = await api
-    .get(`/api/notes/${noteToView.id}`)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+      const contents = await notesAtEnd.map(n => n.content)
+      assert(contents.includes('async/await simplifies making async calls'))
+    })
+  })
 
-  assert.deepStrictEqual(resultNote.body, noteToView)
-})
+  describe('note deleting', () => {
+    test('successed with code 204 if \'id\' is valid', async () => {
+      const notesAtStart = await helper.noteInDb()
+      const noteToDelete = notesAtStart[0]
 
-test('a note can be deleted', async () => {
-  const notesAtStart = await helper.noteInDb()
-  const noteToDelete = notesAtStart[0]
+      await api
+        .delete(`/api/notes/${noteToDelete.id}`)
+        .expect(204)
 
-  await api
-    .delete(`/api/notes/${noteToDelete.id}`)
-    .expect(204)
+      const notesAtEnd = await helper.noteInDb()
 
-  const notesAtEnd = await helper.noteInDb()
-  
-  const contents = notesAtEnd.map(n => n.content)
-  assert(!contents.includes(noteToDelete.content))
+      const contents = notesAtEnd.map(n => n.content)
+      assert(!contents.includes(noteToDelete.content))
 
-  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length - 1)
-})
+      assert.strictEqual(notesAtEnd.length, helper.initialNotes.length - 1)
+    })
+  })
 
-after(async () => {
-  await mongoose.connection.close()
+  after(async () => {
+    await mongoose.connection.close()
+  })
 })
