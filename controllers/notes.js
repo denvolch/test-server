e51ccd999165056ccd316d0e14c7306dea17a57d
denvolch <wolkov.web@yandex.ru>
2024-03-14 13:21:45 +0300

    refactor: populate notes and users db
    using native mongoose function to populating with data

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,8 +1,9 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (req, res) => {
-  const notes = await Note.find({})
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
   res.json(notes)
 })
 
@@ -18,6 +19,7 @@ notesRouter.get('/:id', async (req, res) => {
 
 notesRouter.post('/', async (req, res) => {
   const body = req.body
+  const user = await User.findById(body.userId)
 
   if (!body.content) {
     return res.status(400).json({ error: 'content missing' })
@@ -25,10 +27,14 @@ notesRouter.post('/', async (req, res) => {
 
   const note = new Note({
     content: body.content,
-    important: body.important || false
+    important: body.important || false,
+    user: user.id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+  
   res.status(201).json(savedNote)
 })
 
